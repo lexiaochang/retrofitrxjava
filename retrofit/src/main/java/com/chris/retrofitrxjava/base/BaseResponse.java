252adede96cee66161fcd2854e39498549da420f
ChrisLeGitHub <mrchriscl@icloud.com>
2017-12-14 16:41:22 +0800

    update

---
+++ retrofit/src/main/java/com/chris/retrofitrxjava/base/BaseResponse.java
@@ -0,0 +1,50 @@
+package com.chris.retrofitrxjava.base;
+
+/**
+ * Created by Chris on 2017/12/13.
+ * 网络请求结果 基类
+ */
+
+public class BaseResponse<T> {
+
+
+    /**
+     * dataMap : {"name":"666","age":26}
+     * message : 成功
+     * status : 200
+     */
+
+    private String message;
+    private int    status;
+
+    private T dataMap;
+
+    public boolean isSuccess(){
+        return status == 200;
+    }
+
+
+    public T getDataMap() {
+        return dataMap;
+    }
+
+    public void setDataMap(T dataMap) {
+        this.dataMap = dataMap;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
